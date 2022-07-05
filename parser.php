9ec8fd2c8ef9082b775cebf3089edf1d9ec24d55
enotalexnot <78168667+enotalexnot@users.noreply.github.com>
2022-07-05 12:06:03 +0300

    Add files via upload

---
+++ parser.php
@@ -0,0 +1,51 @@
+<?php 
+require_once 'vendor/autoload.php';
+use BenMorel\ApacheLogParser\Parser;
+
+class ParseLogs {
+	
+    private $parseFile;
+	private $logFormat; 
+	private $parser; 
+    private $parserProcessed;
+	private $parserResult = [];
+	
+	function parse(string $file){
+		$parseFile = file($file);
+		
+		$logFormat = '%h %l %u %t %m %f %h %s %b %U %h';
+		$parser = new Parser($logFormat); 
+		$parserResult['views'] = count($parseFile);
+		$parserResult['urls'] = [];
+		$parserResult['traffic'] = 0;
+		$parserResult['crawlers'] = ['Google' => 0, 'Bing' => 0, 'Baidu' => 0, 'Yandex' => 0];
+		$parserResult['statusCodes'] = [];
+		foreach ($parseFile as $value){
+			
+			$parseResult = $parser->parse($value, false);
+			
+			array_push($parserResult['urls'], $parseResult[5]);
+			
+			if($parseResult[7]=='200'){
+				$parserResult['traffic']+=$parseResult[8];
+				$parserResult['statusCodes']['200']++;
+			}else if($parseResult[7]=='301'){
+				$parserResult['statusCodes']['301']++;
+			}
+			
+			foreach ($parserResult['crawlers'] as $key => $value){
+				
+				if(strpos($parseResult[10],$key)){
+					$parserResult['crawlers'][$key]++;
+				}
+			}
+		}
+		
+		$parserResult['urls'] = count(array_unique($parserResult['urls']));
+		echo $json_parserResult = json_encode($parserResult);
+	
+    }
+}
+$obj = new ParseLogs();
+$obj->parse($argv[1]);
+?>
